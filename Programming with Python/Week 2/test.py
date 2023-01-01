0f19bfcb5a4558f3dd5acf107c47afc284ce491a
Jakub Szpytka <jakub.szpytka@gmail.com>
2023-01-01 17:46:56 +0100

    5561

--- Programming with Python/Week 2/test.py
+++ Programming with Python/Week 2/test.py
@@ -1,4 +1,13 @@
 import os
+import time
+import random
 
-os.system("git add .")
-os.system('git commit -m "test"')
+for i in range(1, 32):
+    with open("testc.txt", "a") as file:
+        randomNumber = random.randint(999, 9999)
+        file.write(f"{randomNumber}")
+        os.system("git add .")
+        os.system(
+            f'git commit -m "{randomNumber}" --date="2023-01-{i if i >= 10 else f"0{i}"}"'
+        )
+    time.sleep(1)
