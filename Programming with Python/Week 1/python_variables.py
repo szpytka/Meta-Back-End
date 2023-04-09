92c4bb80881eaa99b552365c6cc4b8a409f8191b
Jakub Szpytka <jakub.szpytka@gmail.com>
2023-04-09 09:35:03 +0200

    Week 1

---
+++ Programming with Python/Week 1/python_variables.py
@@ -0,0 +1,13 @@
+a = 'this is' \
+    'a multiline string' \
+    'with multilines'
+
+print(a)
+
+b="hello"
+c='there '
+print(b+c)
+
+name = input("Input name: ")
+print(type(name))
+print(name[2])
