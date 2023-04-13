7aeb20a300824b2f380d171aa4b8bc7f99b6e0cb
Jakub Szpytka <jakub.szpytka@gmail.com>
2023-04-13 16:06:25 +0200

    kwargs

--- Programming with Python/Week 2/python_kwargs.py
+++ Programming with Python/Week 2/python_kwargs.py
@@ -1,8 +1,8 @@
-def sum_of(*args):
+def sum_of(**kwargs):
     sum = 0
-    for x in args:
-        sum += x
-    return sum
+    for k, v in kwargs.items():
+        sum += v
+    return round(sum, 2)
 
 
-print(sum_of(4, 5, 6, 4, 5, 6))
+print(sum_of(coffee=2.99, cake=4.55, juice=2.99))
